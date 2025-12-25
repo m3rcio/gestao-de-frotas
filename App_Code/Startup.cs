5b69bd59e90e67b39fe8ad13bc98b3766753a00c
Mércio <mercio20a@gmail.com>
2025-12-25 16:43:01 +0100

    adding files

---
+++ App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(gestaodefrotas.Startup))]
+namespace gestaodefrotas
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
