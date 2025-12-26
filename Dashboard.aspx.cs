269088ec14bca8f941206288e3204204f8d9e97f
Mércio <mercio20a@gmail.com>
2025-12-26 17:26:25 +0100

    adapting the dashboard template to the website

--- Dashboard.aspx.cs
+++ Dashboard.aspx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -17,5 +18,17 @@ public partial class Dashboard : System.Web.UI.Page
             CarregarTabela();
     }
 
+    private void CarregarTabela()
+    {
+        DataTable dt = new DataTable();
+        dt.Columns.Add("Nome");
+        dt.Columns.Add("Cargo");
+        dt.Columns.Add("Prioridade");
+        dt.Columns.Add("Budget");
 
+        dt.Rows.Add("Sunil Joshi", "Web Designer", "Low", "$3.9K");
+
+        gvPerformance.DataSource = dt;
+        gvPerformance.DataBind();
+    }
 }
