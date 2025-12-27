b483cdbf3f82957ecec743e1ea2684ab5023f365
Mércio <mercio20a@gmail.com>
2025-12-27 18:13:09 +0100

    testando inserção de veículos

--- VeiculoForm.aspx.cs
+++ VeiculoForm.aspx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -15,6 +16,31 @@ public partial class VeiculoForm : System.Web.UI.Page
 
     protected void btnSalvar_Click(object sender, EventArgs e)
     {
-        
+        string cs = ConfigurationManager
+                        .ConnectionStrings["DB"].ConnectionString;
+
+        using (SqlConnection con = new SqlConnection(cs))
+        {
+            string sql = @"INSERT INTO Veiculos
+                          (Matricula, Marca, Modelo, Ano, Quilometragem, Estado)
+                           VALUES
+                          (@Matricula, @Marca, @Modelo, @Ano, @Km, @Estado)";
+
+            SqlCommand cmd = new SqlCommand(sql, con);
+
+            cmd.Parameters.AddWithValue("@Matricula", txtMatricula.Text);
+            cmd.Parameters.AddWithValue("@Marca", txtMarca.Text);
+            cmd.Parameters.AddWithValue("@Modelo", txtModelo.Text);
+            cmd.Parameters.AddWithValue("@Ano", int.Parse(txtAno.Text));
+            cmd.Parameters.AddWithValue("@Km", int.Parse(txtKm.Text));
+            cmd.Parameters.AddWithValue("@Estado", txtEstado.Text);
+
+            con.Open();
+            cmd.ExecuteNonQuery();
+        }
+
+        lblMsg.Text = "Veículo inserido com sucesso";
+        lblMsg.Visible = true;
     }
+}
 }
