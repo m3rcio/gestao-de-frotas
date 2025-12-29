3fd8b784f51692650043fa51d3890e5f483e0802
Mércio <mercio20a@gmail.com>
2025-12-29 21:26:28 +0100

    editar e deletar veículos

--- EditarVeiculo.aspx.cs
+++ EditarVeiculo.aspx.cs
@@ -9,6 +9,16 @@ public partial class EditarVeiculo : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        if (!IsPostBack)
+        {
+            if (Request.QueryString["id"] == null)
+            {
+                Response.Redirect("Dashboard.aspx");
+                return;
+            }
 
+            int id = int.Parse(Request.QueryString["id"]);
+            CarregarVeiculo(id);
+        }
     }
 }
