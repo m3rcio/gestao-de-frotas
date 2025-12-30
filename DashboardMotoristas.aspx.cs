8d3ccf34311f98dda5a36cf9df3c23e5901ae1ef
Mércio <mercio20a@gmail.com>
2025-12-30 14:07:02 +0100

    editando o DashboardMotoristas

--- DashboardMotoristas.aspx.cs
+++ DashboardMotoristas.aspx.cs
@@ -13,6 +13,34 @@ public partial class DashboardMotoristas : System.Web.UI.Page
             Response.Redirect("Default.aspx");
 
         if (!IsPostBack)
-            CarregarVeiculos();
+            CarregarMotoristas();
+    }
+
+    private void CarregarMotoristas()
+    {
+        string cs = ConfigurationManager
+                    .ConnectionStrings["DefaultConnection"]
+                    .ConnectionString;
+
+        using (SqlConnection con = new SqlConnection(cs))
+        {
+            string sql = @"SELECT 
+                           veiculo_id,
+                           Marca,
+                           Modelo,
+                           Matricula,
+                           Quilometragem,
+                           Ano,
+                           Estado
+                       FROM Veiculos
+                       ORDER BY veiculo_id DESC";
+
+            SqlDataAdapter da = new SqlDataAdapter(sql, con);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+
+            gvVeiculos.DataSource = dt;
+            gvVeiculos.DataBind();
+        }
     }
 }
