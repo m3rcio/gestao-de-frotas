b4edd0698ac6e5db54328f36cbafeed533d02d1b
Mércio <mercio20a@gmail.com>
2025-12-30 12:53:24 +0100

    adicionando a template ao MotoristaForm

--- MotoristaForm.aspx.cs
+++ MotoristaForm.aspx.cs
@@ -9,6 +9,7 @@ public partial class MotoristaForm : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-
+        if (Session["Logado"] == null)
+            Response.Redirect("Default.aspx");
     }
 }
