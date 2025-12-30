895025a653a09b2d7b64ab4860bf2407c0f5c289
Mércio <mercio20a@gmail.com>
2025-12-30 18:04:33 +0100

    EditarMotorista.aspx

--- EditarMotorista.aspx.cs
+++ EditarMotorista.aspx.cs
@@ -57,4 +57,39 @@ public partial class EditarMotorista : System.Web.UI.Page
             }
         }
     }
+
+    protected void btnAtualizar_Click(object sender, EventArgs e)
+    {
+        int id = int.Parse(Request.QueryString["id"]);
+
+        string cs = ConfigurationManager
+                    .ConnectionStrings["DefaultConnection"]
+                    .ConnectionString;
+
+        using (SqlConnection con = new SqlConnection(cs))
+        {
+            string sql = @"UPDATE Veiculos
+                       SET matricula = @matricula,
+                           marca = @marca,
+                           modelo = @modelo,
+                           ano = @ano,
+                           quilometragem = @km,
+                           estado = @estado
+                       WHERE veiculo_id = @id";
+
+            SqlCommand cmd = new SqlCommand(sql, con);
+            cmd.Parameters.AddWithValue("@matricula", txtMatricula.Text);
+            cmd.Parameters.AddWithValue("@marca", txtMarca.Text);
+            cmd.Parameters.AddWithValue("@modelo", txtModelo.Text);
+            cmd.Parameters.AddWithValue("@ano", int.Parse(txtAno.Text));
+            cmd.Parameters.AddWithValue("@km", int.Parse(txtKm.Text));
+            cmd.Parameters.AddWithValue("@estado", ddlEstado.SelectedValue);
+            cmd.Parameters.AddWithValue("@id", id);
+
+            con.Open();
+            cmd.ExecuteNonQuery();
+        }
+
+        Response.Redirect("Dashboard.aspx");
+    }
 }
