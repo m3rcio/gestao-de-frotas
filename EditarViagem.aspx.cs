57f31377a50018d574f6cc36221c762ad0d60dc7
Mércio <mercio20a@gmail.com>
2025-12-31 17:36:28 +0100

    ...

--- EditarViagem.aspx.cs
+++ EditarViagem.aspx.cs
@@ -21,4 +21,47 @@ public partial class EditarViagem : System.Web.UI.Page
             CarregarViagem(id);
         }
     }
+
+    private void CarregarViagem(int id)
+    {
+        string cs = ConfigurationManager
+                    .ConnectionStrings["DefaultConnection"]
+                    .ConnectionString;
+
+        using (SqlConnection con = new SqlConnection(cs))
+        {
+            string sql = @"
+            SELECT veiculo_id, motorista_id, data_saida, data_retorno,
+                   km_saida, km_retorno
+            FROM Viagens
+            WHERE viagem_id = @id";
+
+            SqlCommand cmd = new SqlCommand(sql, con);
+            cmd.Parameters.AddWithValue("@id", id);
+
+            con.Open();
+            SqlDataReader dr = cmd.ExecuteReader();
+
+            if (dr.Read())
+            {
+                ddlVeiculo.SelectedValue = dr["veiculo_id"].ToString();
+                ddlMotorista.SelectedValue = dr["motorista_id"].ToString();
+
+                txtDataSaida.Text = Convert
+                    .ToDateTime(dr["data_saida"])
+                    .ToString("yyyy-MM-ddTHH:mm");
+
+                if (dr["data_retorno"] != DBNull.Value)
+                {
+                    txtDataRetorno.Text = Convert
+                        .ToDateTime(dr["data_retorno"])
+                        .ToString("yyyy-MM-ddTHH:mm");
+                }
+
+                txtKmSaida.Text = dr["km_saida"].ToString();
+                txtKmRetorno.Text = dr["km_retorno"].ToString();
+            }
+        }
+    }
+
 }
